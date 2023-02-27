aafd6cc444cd3a23a48a82e6405e19f3622e3993
Md. Shahriar Kabir Himel <death44635@gmail.com>
2023-02-27 18:48:33 +0600

    1st task Done

---
+++ ToDoNoteAPI/Data/dbConnection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoNoteAPI.Models;
+
+namespace ToDoNoteAPI.Data
+{
+    public class dbConnection : DbContext
+    {
+        public dbConnection(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ ToDoNoteAPI/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoNoteAPI.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Required]
+        public string noteDescrition { get; set; }
+        public bool isActive { get; set; }
+        
+        [Display(Name = "Attachment")]
+        public string attachedPhoto { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatesDate { get; set; }
+
+    }
+}
